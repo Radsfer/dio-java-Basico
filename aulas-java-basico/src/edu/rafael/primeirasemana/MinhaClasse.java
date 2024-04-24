97bd11441baeb9b1ce22f11592b9d29aaae5a8f6
Rafael Adolfo <rafael.ferreira11.98@gmail.com>
2024-04-24 00:28:14 -0300

    Primeiro dia completo

--- aulas-java-basico/src/edu/rafael/primeirasemana/MinhaClasse.java
+++ aulas-java-basico/src/edu/rafael/primeirasemana/MinhaClasse.java
@@ -3,6 +3,8 @@ package edu.rafael.primeirasemana;
 public class MinhaClasse {
     public static void main(String[] args) {
         System.err.println("Olá Santander!");
+        String nomeCompleto = nomeCompleto("Rafael", "Adolfo");
+        System.out.println(nomeCompleto);
         int media = 8;
         if (media < 6)
             System.out.println("Reprovado!");
@@ -10,5 +12,9 @@ public class MinhaClasse {
             System.out.println("Caso de Minerva");
         else
             System.err.println("Aprovado!");
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Testando funcionalidade: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
