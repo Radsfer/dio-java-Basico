79f3e56901d19d813114dc4847728008979d99fb
Rafael Adolfo <rafael.ferreira11.98@gmail.com>
2024-04-24 00:16:30 -0300

    commit organizando anotações

---
+++ aulas-java-basico/src/edu/rafael/primeirasemana/MinhaClasse.java
@@ -0,0 +1,14 @@
+package edu.rafael.primeirasemana;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.err.println("Olá Santander!");
+        int media = 8;
+        if (media < 6)
+            System.out.println("Reprovado!");
+        else if (media == 6)
+            System.out.println("Caso de Minerva");
+        else
+            System.err.println("Aprovado!");
+    }
+}
